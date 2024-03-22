f96beabd44a6b60d08433e2aeb9f7ec96bd2430e
alda-1995 <aldair@canalla.agency>
2024-03-22 13:14:42 -0600

    Agregar archivos de proyecto.

---
+++ PatronStrategy/Program.cs
@@ -0,0 +1,13 @@
+﻿using PatronStrategy;
+
+var context = new Context();
+
+Console.WriteLine("Client: Strategy is set to normal sorting.");
+context.SetStrategy(new ConcreteStrategyA());
+context.DoSomeBussinessLogic();
+
+Console.WriteLine("");
+
+Console.WriteLine("Client: Strategy is set to reverse sorting.");
+context.SetStrategy(new ConcreteStrategyB());
+context.DoSomeBussinessLogic();
